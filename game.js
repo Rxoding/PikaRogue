d83abf79c45dfdf263e8bcb97f0d727734869719
Rxoding <neumreum0419@naver.com>
2024-08-28 13:31:32 +0900

    최종 커밋

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import readlineSync from "readline-sync";
 
+// 피카츄
 class Player {
   constructor() {
     this.name = "피카츄";
@@ -27,17 +28,18 @@ class Player {
   attackWithAccuracy(skillName, monster, damageMultiplier) {
     const hitChance = this.acc[skillName];
     const randomChance = Math.round(Math.random() * 100);
+    const skillDamage = this.damage * damageMultiplier;
 
     if (randomChance <= hitChance) {
-      monster.hp -= this.damage * damageMultiplier;
-      return true; // 공격 성공
+      monster.hp -= skillDamage;
+      return { success: true, skillDamage: skillDamage }; // 공격 성공, 피해량 반환
     } else {
-      return false; // 공격 실패
+      return { success: false }; // 공격 실패
     }
   }
 
   quickAttack(monster) {
-    return this.attackWithAccuracy("quickAttack", monster, 10000);
+    return this.attackWithAccuracy("quickAttack", monster, 1);
   }
 
   thunderShock(monster) {
@@ -57,6 +59,7 @@ class Player {
   }
 }
 
+// 야생 몬스터
 class Monster {
   constructor(stage) {
     const monsterNames = [
@@ -74,8 +77,8 @@ class Monster {
     this.hp = 100 * stage;
     this.damage = 10 * stage;
     this.skills = {
-      tackle: { name: "몸통박치기", damageMultiplier: 1, accuracy: 95 },
-      leafCutter: { name: "잎날가르기", damageMultiplier: 2, accuracy: 85 },
+      tackle: { name: "몸통박치기", damageMultiplier: 1, accuracy: 90 },
+      leafCutter: { name: "잎날가르기", damageMultiplier: 2, accuracy: 80 },
       vineWhip: { name: "풀묶기", damageMultiplier: 3, accuracy: 70 },
       solarBeam: { name: "솔라빔", damageMultiplier: 5, accuracy: 50 },
     };
@@ -107,26 +110,32 @@ class Monster {
   }
 }
 
+// 딜레이 함수
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// 배틀 상태창
 function displayStatus(stage, player, monster) {
-  console.log(chalk.magentaBright(`\n=== Current Status ===`));
+  console.log(
+    chalk.magentaBright(`\n${"=".repeat(32)} 배틀 상태 ${"=".repeat(32)}`)
+  );
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
       chalk.blueBright(
-        `| ${player.name}의 HP : ${player.hp}, Level : ${player.level}`
+        `| ${player.name}의 HP : ${player.hp}, Level : ${player.level} `
       ) +
       chalk.redBright(
         `| ${monster.name}의 HP : ${monster.hp}, 공격력 : ${monster.damage} |`
       )
   );
-  console.log(chalk.magentaBright(`=====================\n`));
+  console.log(chalk.magentaBright(`${"=".repeat(75)}\n`));
 }
 
+// 포켓몬 배틀
 const battle = async (stage, player, monster) => {
   let logs = [];
+  let boss = false; // 최종 보스 알림 여부
 
   while (player.hp > 0 && monster.hp > 0) {
     console.clear();
@@ -134,115 +143,181 @@ const battle = async (stage, player, monster) => {
 
     logs.forEach((log) => console.log(log));
 
+    // 최종 보스 메시지 출력 (단 한 번만)
+    if (stage === 10 && !boss) {
+      logs.push(chalk.redBright("!!! 최종 보스와의 전투입니다 !!!"));
+      console.log(logs[logs.length - 1]);
+      await delay(1000); // 1초 대기
+      boss = true; // 메시지 출력 후 상태 변경
+    }
+
     console.log(
       chalk.green(
-        `\n1. 전광석화(${player.damage}) 2. 전기쇼크(${player.damage * 2}) 3. 스파크(${player.damage * 3}) 4. 10만볼트(${player.damage * 4}) 5. 도망간다!`
+        `\n1. 전광석화(${player.damage}) 2. 전기쇼크(${player.damage * 2}) 3. 스파크(${player.damage * 3}) 4. 10만볼트(${player.damage * 5}) 5. 도망간다!`
       )
     );
     const choice = readlineSync.question("당신의 선택은? ");
 
-    let attackSuccessful = false;
+    let attackResult;
     let vaildChoice = true;
     switch (choice) {
       case "1":
-        logs.push(chalk.green(`가랏! ${player.name}! 전광석화!!!`));
+        logs.push(chalk.green(`\n가랏! ${player.name}! 전광석화!!!`));
         console.log(logs[logs.length - 1]);
         await delay(500);
-        attackSuccessful = player.quickAttack(monster);
+        attackResult = player.quickAttack(monster);
         break;
       case "2":
-        logs.push(chalk.green(`가랏! ${player.name}! 전기쇼크!!!`));
+        logs.push(chalk.green(`\n가랏! ${player.name}! 전기쇼크!!!`));
         console.log(logs[logs.length - 1]);
         await delay(500);
-        attackSuccessful = player.thunderShock(monster);
+        attackResult = player.thunderShock(monster);
         break;
       case "3":
-        logs.push(chalk.green(`가랏! ${player.name}! 스파크!!!`));
+        logs.push(chalk.green(`\n가랏! ${player.name}! 스파크!!!`));
         console.log(logs[logs.length - 1]);
         await delay(500);
-        attackSuccessful = player.spark(monster);
+        attackResult = player.spark(monster);
         break;
       case "4":
-        logs.push(chalk.green(`가랏! ${player.name}! 10만볼트!!!`));
+        logs.push(chalk.green(`\n가랏! ${player.name}! 10만볼트!!!`));
         console.log(logs[logs.length - 1]);
         await delay(500);
-        attackSuccessful = player.thunderBolt(monster);
+        attackResult = player.thunderBolt(monster);
         break;
-      case "5":
-        logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
-
+      case "5": // 도망치기
+        if (stage === 10) {
+          logs.push(chalk.redBright("\n최종보스는 도망갈 수 없습니다!"));
+          console.log(logs[logs.length - 1]);
+          vaildChoice = false; // 도망 시도 실패로 처리
+          break; // switch 문을 벗어나기 위해 break 추가
+        }
         if (player.run() === true) {
-          console.log(chalk.white("무사히 도망쳤다!"));
+          console.log(chalk.white("\n무사히 도망쳤다!"));
           await delay(1000);
           return true; // 도망 성공 시 true 반환
         } else {
-          logs.push(chalk.white("도망칠 수 없다!"));
+          logs.push(chalk.redBright("\n도망칠 수 없다!"));
           console.log(logs[logs.length - 1]);
           vaildChoice = false;
+          break;
         }
       default:
-        logs.push(chalk.red("잘못된 선택입니다."));
+        logs.push(chalk.red("\n잘못된 선택입니다."));
         vaildChoice = false;
     }
 
     if (vaildChoice && choice !== "5") {
       await delay(500);
-      if (attackSuccessful) {
-        logs.push(chalk.white(`${player.name}의 공격이 명중했습니다!`));
+      if (attackResult.success) {
+        logs.push(chalk.white(`${player.name}의 공격이 명중했습니다! `));
+        logs.push(
+          chalk.white(`${attackResult.skillDamage}의 피해를 입혔습니다!`)
+        );
+        // 몬스터의 HP가 0 이하로 떨어진 경우
+        if (monster.hp <= 0) {
+          logs.push(chalk.blueBright(`야생의 ${monster.name}이(가) 쓰러졌다!`));
+          await delay(500);
+          return false; // 배틀 종료
+        }
       } else {
         logs.push(chalk.red(`${player.name}의 공격이 빗나갔습니다...`));
       }
     }
-    const monsterAttack = monster.attack(player);
-    await delay(500);
-    if (monsterAttack.success) {
-      logs.push(
-        chalk.red(
-          `야생의 ${monster.name}이(가) ${monsterAttack.skillName}를 사용했다! ${monsterAttack.damage}의 피해를 입었다!`
-        )
-      );
-    } else {
-      logs.push(
-        chalk.red(
-          `야생의 ${monster.name}이(가) ${monsterAttack.skillName}를 사용했지만 빗나갔다!`
-        )
-      );
-    }
-    // 공격 후 상태 출력
-    logs.push(chalk.blueBright(`${player.name}의 남은 체력: ${player.hp}`));
-    logs.push(chalk.redBright(`${monster.name}의 남은 체력: ${monster.hp}`));
-  }
-  // 게임중 죽었다면
-  if (player.hp <= 0) {
-    console.clear();
-    console.log(chalk.red(`${player.name}은(는) 죽었습니다...`));
-    console.log(chalk.yellow("게임을 재시작하려면 1을 누르세요."));
-    console.log(chalk.yellow("게임을 포기하려면 2를 누르세요."));
-    const choice = readlineSync.question("당신의 선택은? ");
 
-    if (choice === "1") {
-      console.log(chalk.green("게임을 재시작합니다..."));
-      return true; // 재시작 선택 시 true 반환
-    } else {
-      console.log(chalk.green("게임을 종료합니다. 감사합니다!"));
-      return false; // 포기 선택 시 false 반환
+    // 몬스터가 플레이어를 공격
+    if (player.hp > 0 && monster.hp > 0) {
+      // 플레이어와 몬스터 모두 생존 시
+      await delay(500);
+      const monsterAttack = monster.attack(player);
+      if (monsterAttack.success) {
+        logs.push(
+          chalk.red(
+            `야생의 ${monster.name}이(가) ${monsterAttack.skillName}를 사용했다! ${monsterAttack.damage}의 피해를 입었다!`
+          )
+        );
+        if (player.hp <= 0) {
+          await delay(500);
+          logs.push(chalk.redBright(`\n${player.name}은(는) 쓰러졌다...!`));
+          await delay(500);
+          logs.push(chalk.white(`당신은 눈앞이 깜깜해졌다!`));
+          await delay(500);
+          console.clear();
+          displayStatus(stage, player, monster);
+          logs.forEach((log) => console.log(log));
+          console.log(chalk.yellow("피카로그를 재도전 하실꺼면 1을 누르세요."));
+          console.log(chalk.yellow("피카로그를 포기하려면 2를 누르세요."));
+          const choice = readlineSync.question("당신의 선택은? ");
+
+          if (choice === "1") {
+            console.log(chalk.green("피카로그를 재도전 합니다..."));
+            await delay(500);
+            console.log(chalk.green("Good Luck...!"));
+            await delay(1500);
+            return "restart"; // 재시작 선택 시 "restart" 반환
+          } else {
+            console.log(chalk.green("게임을 종료합니다. 감사합니다!"));
+            return "quit"; // 포기 선택 시 "quit" 반환
+          }
+        }
+      } else {
+        logs.push(
+          chalk.red(
+            `야생의 ${monster.name}이(가) ${monsterAttack.skillName}를 사용했지만 빗나갔다!`
+          )
+        );
+      }
     }
+
+    logs.push(
+      chalk.blueBright(`| ${player.name}의 HP : ${player.hp} `) +
+        chalk.redBright(`| ${monster.name}의 HP : ${monster.hp}|`)
+    );
   }
 
   return false; // 배틀이 끝난 후 도망치지 않았다면 false 반환
 };
 
+// 타이핑 효과 함수
+async function typeEffect(text, delayMs = 100) {
+  for (let char of text) {
+    process.stdout.write(char); // 한 글자씩 출력
+    await delay(delayMs); // 지정된 시간만큼 대기
+  }
+  console.log(); // 마지막에 줄바꿈
+}
+
+// 게임 시작
 export async function startGame() {
   console.clear();
+
+  // 인트로 메시지 추가
+  await typeEffect(
+    "당신은 앞으로 피카츄와 함께 10개의 스테이지를 도전하게됩니다.\n",
+    50
+  );
+  await delay(1000); // 1초 대기 후 게임 시작
+  await typeEffect(
+    "10개의 스테이지를 클리어하고 최고의 포켓몬에 도전하세요!\n",
+    50
+  );
+  await delay(1000); // 1초 대기 후 게임 시작
+  await typeEffect("그럼, 행운을 빕니다! Good Luck!\n", 50);
+  await delay(2000); // 1초 대기 후 게임 시작
+
   const player = new Player();
   let stage = 1;
 
   while (stage <= 10 && player.hp > 0) {
     const monster = new Monster(stage);
 
-    const runAway = await battle(stage, player, monster);
+    const result = await battle(stage, player, monster);
 
-    if (!runAway) {
+    if (result === "restart") {
+      return startGame(); // 게임 재시작
+    } else if (result === "quit") {
+      return; // 게임 종료
+    } else if (!result) {
       console.log(chalk.blue(`야생의 ${monster.name}이(가) 쓰러졌다!`));
       await delay(500);
 
@@ -255,19 +330,19 @@ export async function startGame() {
       );
       await delay(1500);
 
-      // 진화 체크 및 진화 메시지 출력
+      // 진화 및 진화 메시지 출력
       if (player.level === 5 && !player.isEvolved) {
-        await delay(1000);
-        console.log("어?... 피카츄의 상태가...?");
+        console.clear();
+        console.log("....  오잉!?");
         await delay(500);
-        console.log("...");
+        console.log("피카츄의 상태가...!");
         await delay(500);
         console.log("...");
         await delay(500);
         console.log("...");
         await delay(1000);
         console.log(
-          chalk.yellow("축하합니다! 피카츄는 라이츄로 진화했습니다!!")
+          chalk.yellow("\n축하합니다! 피카츄는 라이츄로 진화했습니다!!")
         );
         player.name = "라이츄";
         player.isEvolved = true;
@@ -279,7 +354,7 @@ export async function startGame() {
     if (stage < 10) {
       // 새로운 스테이지 메시지
       console.clear();
-      console.log(chalk.red(`야생의 몬스터가 나타났다!`));
+      console.log(chalk.red(`\n\n야생의 몬스터가 나타났다!`));
       await delay(1500);
       stage++;
     } else {
